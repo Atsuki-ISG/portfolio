51d364b055466afa6fc11c7c799025869e92fa03
Atsuki-ISG <142329658+Atsuki-ISG@users.noreply.github.com>
2023-11-10 06:35:23 +0900

    Delete LetsEngineer directory

--- LetsEngineer/curriculum/PHPjob/practice/form1/result.php
+++
@@ -1,8 +0,0 @@
-<?php
-$my_name = $_POST['my_name'];
-$mailAdress = $_POST['mailadress'];
-$password = $_POST['password'];
-?>
-<p>私の名前は、<?php echo $my_name; ?></p>
-<p>メールアドレスは、<?php echo $mailAdress; ?></p>
-<p>私のパスワードは、<?php echo $password; ?></p>
